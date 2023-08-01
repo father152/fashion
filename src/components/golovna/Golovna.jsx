e05b6e9ebe28ee53b96178ebc2fc1ff3e24df465
Your Name <tsimbalenko.vv@gmail.com>
2023-08-01 13:53:28 +0200

    6

--- src/components/golovna/Golovna.jsx
+++ src/components/golovna/Golovna.jsx
@@ -1,7 +1,18 @@
-import { Link } from 'react-router-dom';
-import './golovna.css'
+import { Link } from "react-router-dom";
+import "./golovna.css";
 import image from "./girl.png";
 import Image from "./LET’S EXPLORE UNIQUE CLOTHES..png";
+import icon1 from "./Rectangle 36.png";
+import icon2 from "./Rectangle 44.png";
+import icon3 from "./Rectangle 38.png";
+import icon4 from "./Rectangle 45.png";
+import icon5 from "./Rectangle 43.png";
+import icon6 from "./Rectangle 41.png";
+import icon7 from "./NEW ARRIVALS.png";
+import photo1 from './Rectangle 20.png'
+import photo2 from './Rectangle 21.png'
+import photo3 from './Rectangle 22.png'
+import arrow from './arrow-right-svgrepo-com.svg'
 
 function Golovna() {
   return (
@@ -10,12 +21,55 @@ function Golovna() {
         <div className="leftpart">
           <img src={Image} alt=""></img>
           <p>Live for Influential and Innovative fashion!</p>
-          <button className='shopnow'>
-           <Link to="/shopnow">SHOP NOW</Link>
-           </button>
+          <button className="shopnow">
+            <Link to="/shopnow">SHOP NOW</Link>
+          </button>
         </div>
         <div className="rigthpart">
           <img src={image} alt=""></img>
+        </div>
+      </div>
+      <div className="container-2">
+        <Link to="/h&m">
+          <img src={icon1} alt=""></img>
+        </Link>
+        <Link to="/obey">
+          <img src={icon2} alt=""></img>
+        </Link>
+        <Link to="/shopify">
+          <img src={icon3} alt=""></img>
+        </Link>
+        <Link to="/lacoste">
+          <img src={icon4} alt=""></img>
+        </Link>
+        <Link to="/levis">
+          <img src={icon5} alt=""></img>
+        </Link>
+        <Link to="/amazon">
+          <img src={icon6} alt=""></img>
+        </Link>
+      </div>
+      <div className="container-3">
+        <img src={icon7} alt=""></img>
+        <div className='cards2'>
+        <div className='card2'>
+        <img className='foto' src={photo1} alt=""></img>
+        <h3>Hoodies & Sweatshirt</h3>
+        <h4>Explore Now!</h4>
+        <Link to="/hoodiessweatshirt"><img className='arrow' src={arrow} alt=''/></Link>
+        </div>
+        <div className='card2'>
+        <img className='foto' src={photo2} alt=""></img>
+        <h3>Coats & Parkas</h3>
+        <h4>Explore Now!</h4>
+        <Link to="/coatsparkas"><img className='arrow' src={arrow} alt=''/></Link>
+        </div>
+        <div className='card2'>
+        <img className='foto' src={photo3} alt=""></img>
+        <h3>Tees & T-Shirt</h3>
+        <h4>Explore Now!</h4>
+        <Link to="/teesshirt"><img className='arrow' src={arrow} alt=''/></Link>
+        </div>
         </div>
       </div>
     </div>
