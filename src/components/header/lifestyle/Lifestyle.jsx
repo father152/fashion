0c5e313452a03ec6d2308c8929af3fa09a32922d
Your Name <tsimbalenko.vv@gmail.com>
2023-08-02 11:11:01 +0200

    1

--- src/components/header/lifestyle/Lifestyle.jsx
+++ src/components/header/lifestyle/Lifestyle.jsx
@@ -1,8 +1,37 @@
+import "./lifestyle.css";
+
 const Lifestyle = () => {
-    return (
-        <div className='Lifestyle'>
-        Lifestyle
+  return (
+    <div className="lifestyle">
+       <h5>LIFESTYLE</h5>
+    <div className="part-1">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQildSPqF7fsiWelX8CjF5KCnHoGngDBZinQw&usqp=CAU"></img>
+       
+        <p>
+          Photos are one of the most important elements of your online store.
+          Half of online shoppers use images to decide what to buy online.
+          Lifestyle photography shows your products being used in an artistic
+          manner by your target market in their natural state. Also referred to
+          as candid shots, this style of ecommerce photography helps online
+          shoppers see your products used in a model’s day to day life.
+        </p>
+      </div>
+      <h5>LIFESTYLE</h5>
+      
+      <div className="part-2">
+      <p>
+          Photos are one of the most important elements of your online store.
+          Half of online shoppers use images to decide what to buy online.
+          Lifestyle photography shows your products being used in an artistic
+          manner by your target market in their natural state. Also referred to
+          as candid shots, this style of ecommerce photography helps online
+          shoppers see your products used in a model’s day to day life.
+        </p>
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQildSPqF7fsiWelX8CjF5KCnHoGngDBZinQw&usqp=CAU"></img>
+       
         </div>
-    )
-}
-export  {Lifestyle}
+      <div className="part-3"></div>
+    </div>
+  );
+};
+export { Lifestyle };
