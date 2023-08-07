1a6a3fb943c5f5d34dfc5f4474a24a6173434493
Your Name <tsimbalenko.vv@gmail.com>
2023-08-07 16:30:38 +0200

    1

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,12 +1,14 @@
 import "./footer.css";
 import logo1 from './50.png';
+import { Link } from "react-router-dom";
 
 
 function Footer() {
   return (
     <div className="footer">
       <div className="footer__logo">
-        <img src={logo1} alt=""></img>
+      <Link to='/'><img src={logo1} alt=""></img></Link>
+        
       </div>
       <p1>2021-2023 All Right Reserved </p1>
     </div>
