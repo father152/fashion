183149e2d4550e3fa38c24d0e5631d32d42fdb35
Your Name <tsimbalenko.vv@gmail.com>
2024-06-04 08:51:02 +0300

    4

--- src/components/golovna/Buyhs.jsx
+++ src/components/golovna/Buyhs.jsx
@@ -1,21 +1,10 @@
 import React from "react";
-import { Label } from "./Label";
-import { Season } from "./Season";
-import { Category } from "./Category";
+
 
 function Buyhs() {
   return (
     <div className="buyhs">
-      Buyhs
-      <Label />
-      <Season />
-      <Category />
-      <div className="forms">
-      <hr></hr>
-        LABEL: <form1>52</form1><hr></hr>
-        SEASON: <form2>53</form2><hr></hr>
-        CATEGORY: <form3>54</form3><hr></hr>
-      </div>
+    
     </div>
   );
 }
