02f1d0d61b464af747247d3f2e5dc514643f9fda
Your Name <tsimbalenko.vv@gmail.com>
2024-09-19 13:00:51 +0300

    45

--- src/components/header/lifestyle/Lifestyle.jsx
+++ src/components/header/lifestyle/Lifestyle.jsx
@@ -42,6 +42,18 @@ const Lifestyle = () => {
           shoppers see your products used in a model’s day to day life.
         </p>
       </div>
+      <h5>LIFESTYLE</h5>
+      <div className="part-4">
+                <p>
+          Photos are one of the most important elements of your online store.
+          Half of online shoppers use images to decide what to buy online.
+          Lifestyle photography shows your products being used in an artistic
+          manner by your target market in their natural state. Also referred to
+          as candid shots, this style of ecommerce photography helps online
+          shoppers see your products used in a model’s day to day life.
+        </p>
+       <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRK12xMwhGqSE1d1gEZ7xjuRBbevwrdvXj_yQ&usqp=CAU' alt="pic"></img>
+      </div>
       <Link to="/" className="back">
       <img src={home} alt=""></img>
       
