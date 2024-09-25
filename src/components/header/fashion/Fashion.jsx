1cfa852055e0601be0557c1806e4260967f58830
Your Name <tsimbalenko.vv@gmail.com>
2024-09-25 14:22:17 +0300

    12

--- src/components/header/fashion/Fashion.jsx
+++ src/components/header/fashion/Fashion.jsx
@@ -1,11 +1,24 @@
-
+import "./fashion.css"
 
 
 const Fashion = () => {
     return (
         <div className='fashion'>
-        Fashion
-
+        <div className='name-fashion'>
+        <h1>FASHION</h1>
+        </div>
+<div className='container-fashion'>
+        <div className='news1'>
+        <p>News 1</p>
+        </div>
+        <div className='news2'>
+        <p>News 2</p>
+        </div>
+        <div className='news3'>
+        <p>News 3</p>
+        </div>
+</div>
+        
         </div>
     )
 }
